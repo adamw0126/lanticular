926609cba4c44d2e3ff211cda04c46669acad758
adamw0126 <adamwilson0126@gmail.com>
2024-11-30 08:56:19 +0900

    add: backend

---
+++ backend/models/Admin.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const adminsSchema = new mongoose.Schema({
+    name: { type: String, default: '', required: true },
+    userId: { type: String, default: '', unique : true, required: true },
+    password: { type: String, default: '', required: true },
+    currentImg: { type: String, default: '' },
+    depthImage: { type: String, default: '' },
+    w_h: {
+        width: { type: Number, default: 0 },
+        height: { type: Number, default: 0 }
+    },
+    isLogin: { type: Boolean, default: false },
+    joinedAt: { type: Date, default: Date.now }
+});
+const Admin = mongoose.model('Admin', adminsSchema);
+
+module.exports = Admin;
